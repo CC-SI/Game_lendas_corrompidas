153926c953a674a91ef46737543765427ee6ba1a
Oliverchristian302 <oliverchristian302@gmail.com>
2025-03-18 01:55:08 -0300

    Update player.gd

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -1,16 +1,20 @@
 extends KinematicBody2D
 
+var velocidade = 450
+var direcao = Vector2.ZERO
+var forca_pulo = 1000
+var velocidade_queda = 30 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	direcao.x = 0
+	direcao.y += velocidade_queda
+	
+	if (Input.is_action_pressed("ui_left")):
+		direcao.x = -velocidade
+	elif (Input.is_action_pressed("ui_right")):
+		direcao.x = velocidade
+		
+	if (Input.is_action_just_pressed("ui_up") and is_on_floor()):
+		direcao.y = -forca_pulo
+	
+	direcao = move_and_slide(direcao, Vector2(0,-1))
