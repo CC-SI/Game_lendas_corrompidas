9270f42a7d3d2e70d2228d2912e09a9d1aafc12b
= <camposdlucasoli@gmail.com>
2025-04-04 23:02:47 -0300

    feat: classe dos inimigos

---
+++ src/scripts/Inimigos/MonstroDasLavas.gd
@@ -0,0 +1,5 @@
+extends InimigoBase
+
+func morrer():
+	print("Monstro das lavas morreu, fez barulho de morte!")
+	queue_free()
---
+++ src/scripts/Inimigos/Morcego.gd
@@ -0,0 +1,5 @@
+extends InimigoBase
+
+func morrer():
+	print("Morcego fez barulho de morte!")
+	queue_free()
