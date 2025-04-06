2a3dd3ec33fd4d5e3c583844971a27b56d94bca6
= <camposdlucasoli@gmail.com>
2025-04-06 12:57:15 -0300

    feat : mosntro disparando blocos de fogo. Blocos de fogo sem particulas até o momento

--- src/scripts/Inimigos/BlocoDeFogo.gd
+++ src/scripts/Inimigos/BlocoDeFogo.gd
@@ -1,19 +1,47 @@
 extends KinematicBody2D
 
-export var velocidade_x = 300
-export var gravidade = 1200
-export var forca_pulo = -800
+var velocity = Vector2()
+var gravity = 600
+var jump_force = -400
+var has_landed = false
+var ground_y = 0
 
-var velocidade  = Vector2.ZERO
+onready var timer_destruir = Timer.new()
 
 func _ready():
-	velocidade.x = velocidade_x
-	velocidade.y = forca_pulo
+	randomize()
+
+	timer_destruir.wait_time = 5.0
+	timer_destruir.one_shot = true
+	timer_destruir.connect("timeout", self, "_destruir")
+	add_child(timer_destruir)
+	timer_destruir.start()
+
+	ground_y = position.y
+
+	if velocity == Vector2.ZERO:
+		var direction = 1
+		if randf() < 0.5:
+			direction = -1
+
+		var side_push = rand_range(40, 120) * direction
+		velocity = Vector2(side_push, jump_force)
+
+func set_direcao(nova_direcao):
+	velocity = nova_direcao.normalized() * 200
 
 func _process(delta):
-	velocidade.y += gravidade * delta
-	velocidade = move_and_slide(velocidade)
-	yield(get_tree().create_timer(3.0), "timeout")
+	if has_landed:
+		return
+
+	velocity.y += gravity * delta
+	position += velocity * delta
+
+	if position.y >= ground_y:
+		position.y = ground_y
+		has_landed = true
+
+func _destruir():
 	queue_free()
 
 
--- src/scripts/Inimigos/MonstroDasCinzas.gd
+++ src/scripts/Inimigos/MonstroDasCinzas.gd
@@ -7,22 +7,32 @@ var perseguindo = false
 var alvo = null
 var tempo_no_alvo = 0
 var atirando = false
+var ataque_realizado = false 
 
 onready var timer_tiro = Timer.new()
+onready var timer_ataque = Timer.new()
 
 func _ready():
 	$Sprite.flip_h = true
 	velocidade = 30
+
 	add_child(timer_tiro)
 	timer_tiro.wait_time = intervalo_tiro
 	timer_tiro.connect("timeout", self, "_atirar")
 
+	add_child(timer_ataque)
+	timer_ataque.wait_time = 3.0
+	timer_ataque.one_shot = true
+	timer_ataque.connect("timeout", self, "_fim_do_ataque")
+
 func _process(delta):
-	if (perseguindo and alvo and alvo.is_inside_tree()):
+	if perseguindo and alvo and alvo.is_inside_tree():
 		tempo_no_alvo += delta
-		if tempo_no_alvo >= 3.0 and not atirando:
+
+		# Ataca uma única vez após 3s
+		if tempo_no_alvo >= 3.0 and not atirando and not ataque_realizado:
 			iniciar_ataque()
-		
+
 		if not atirando:
 			var direcao = (alvo.global_position - global_position).normalized()
 			direcao.y = 0
@@ -47,7 +57,9 @@ func _on_ZonaVisao_body_exited(body):
 
 func iniciar_ataque():
 	atirando = true
+	ataque_realizado = true 
 	timer_tiro.start()
+	timer_ataque.start()
 	print("Monstro começou a atirar!")
 
 func parar_ataque():
@@ -55,9 +67,30 @@ func parar_ataque():
 	timer_tiro.stop()
 	print("Monstro parou de atirar.")
 
+func _fim_do_ataque():
+	parar_ataque()
+
 func _atirar():
-	if not alvo: return
-	
-	var bola = bolaDeFogo.instance()
-	get_parent().add_child(bola)
-	bola.global_position = global_position
+	if not alvo:
+		return
+
+	var quantidade = 5
+	var angulo_inicial = -0.4
+	var angulo_final = 0.4
+
+	var direcao_base = (alvo.global_position - global_position).normalized()
+
+	for i in range(quantidade):
+		var t = float(i) / (quantidade - 1)
+		var angulo = lerp(angulo_inicial, angulo_final, t)
+		var nova_direcao = direcao_base.rotated(angulo)
+
+		var bola = bolaDeFogo.instance()
+		get_parent().add_child(bola)
+
+		var pos_offset = nova_direcao * 16
+		bola.global_position = global_position + pos_offset
+
+		if bola.has_method("set_direcao"):
+			bola.set_direcao(nova_direcao)
+
