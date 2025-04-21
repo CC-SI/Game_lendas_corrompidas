0e4e2d93ecf0e33a0d5db75c35bb7261cee632f3
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-04-20 21:32:40 -0300

    fix: chamada do nó AnimationTree na animação do morcego

--- src/scripts/Inimigos/Morcego/animacao_morcego.gd
+++ src/scripts/Inimigos/Morcego/animacao_morcego.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-var morcego = get_parent()
+onready var morcego = get_parent()
 
 func _process(delta):
-	$AnimationTree.set("parameters/conditions/esta_voando", !morcego.esta_atacando and !morcego.esta_morto)
-	$AnimationTree.set("parameters/conditions/esta_atacando", morcego.esta_atacando and !morcego.esta_morto)
-	$AnimationTree.set("parameters/conditions/esta_morto", morcego.esta_morto)
+	$"../AnimationTree".set("parameters/conditions/esta_voando", !morcego.esta_atacando and !morcego.esta_morto)
+	$"../AnimationTree".set("parameters/conditions/esta_atacando", morcego.esta_atacando and !morcego.esta_morto)
+	$"../AnimationTree".set("parameters/conditions/esta_morto", morcego.esta_morto)
