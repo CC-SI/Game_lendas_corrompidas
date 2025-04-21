b5412a4a73651c1b85d24f63bc8abcdfb0732636
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-04-20 21:39:30 -0300

    update: alterando o "estado_jogador" para "padrao" após setagem das condições de animação do player

--- src/scripts/Player/animacao_player.gd
+++ src/scripts/Player/animacao_player.gd
@@ -10,3 +10,5 @@ func _process(delta):
 	$"../AnimationTree".set("parameters/conditions/esta_mordendo", player.estado_jogador == "mordida")
 	$"../AnimationTree".set("parameters/conditions/esta_uivando", player.estado_jogador == "uivo")
 	$"../AnimationTree".set("parameters/conditions/esta_morto", player.estado_jogador == "morto")
+	
+	player.estado_jogador = "padrao"
