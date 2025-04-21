daa534c047a73d5845df33687f4acc0e70295b70
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-04-20 21:32:15 -0300

    fix: chamada do nó AnimationTree na animação do player

--- src/scripts/Player/animacao_player.gd
+++ src/scripts/Player/animacao_player.gd
@@ -1,11 +1,12 @@
 extends Node2D
 
-var player = get_parent()
+onready var player = get_parent()
 
 func _process(delta):
-	$AnimationTree.set("parameters/movimentacao/blend_position", player.direcao.normalized())
+	var direcao = player.direcao.normalized()
+	$"../AnimationTree".set("parameters/movimentacao/blend_position", Vector2(direcao.x if player.is_on_floor() else 0, -1 if !player.is_on_floor() and direcao.y == 0 else direcao.y))
 	
-	$AnimationTree.set("parameters/conditions/dash", player.estado_jogador == "dash")
-	$AnimationTree.set("parameters/conditions/esta_mordendo", player.estado_jogador == "mordida")
-	$AnimationTree.set("parameters/conditions/esta_uivando", player.estado_jogador == "uivo")
-	$AnimationTree.set("parameters/conditions/esta_morto", player.estado_jogador == "morto")
+	$"../AnimationTree".set("parameters/conditions/dash", player.estado_jogador == "dash")
+	$"../AnimationTree".set("parameters/conditions/esta_mordendo", player.estado_jogador == "mordida")
+	$"../AnimationTree".set("parameters/conditions/esta_uivando", player.estado_jogador == "uivo")
+	$"../AnimationTree".set("parameters/conditions/esta_morto", player.estado_jogador == "morto")
