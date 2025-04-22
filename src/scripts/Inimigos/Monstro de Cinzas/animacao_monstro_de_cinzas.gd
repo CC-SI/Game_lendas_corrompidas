9b3d50c719f1f716b84c2f9829f601c1a152b338
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-04-22 19:21:48 -0300

    fix: condição para garantir que a animação de andar não toque se o monstro de cinzas não estiver perseguindo

--- src/scripts/Inimigos/Monstro de Cinzas/animacao_monstro_de_cinzas.gd
+++ src/scripts/Inimigos/Monstro de Cinzas/animacao_monstro_de_cinzas.gd
@@ -5,7 +5,7 @@ onready var monstro_de_cinzas = get_parent()
 func _process(delta):
 	var direcao = monstro_de_cinzas.direcao.normalized()
 	
-	$"../AnimationTree".set("parameters/movimentacao/blend_position", Vector2(direcao.x, 0))
+	$"../AnimationTree".set("parameters/movimentacao/blend_position", Vector2(direcao.x if monstro_de_cinzas.perseguindo else 0, 0))
 	
 	$"../AnimationTree".set("parameters/conditions/esta_movimentando", !monstro_de_cinzas.atirando and !monstro_de_cinzas.esta_morto)
 	$"../AnimationTree".set("parameters/conditions/esta_atacando", monstro_de_cinzas.atirando and !monstro_de_cinzas.esta_morto)
