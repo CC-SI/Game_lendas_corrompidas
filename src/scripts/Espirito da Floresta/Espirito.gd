c4bdf092c51c83829b24dd56501564dced679be2
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-04-28 00:46:52 -0300

    refactor: refatorando lógica de diálogo do espirito e passando as falas por animação

--- src/scripts/Espirito da Floresta/Espirito.gd
+++ src/scripts/Espirito da Floresta/Espirito.gd
@@ -7,41 +7,47 @@ var indice_fala = 0
 var jogador_perto = false
 var dialogo_ativo = false
 
+onready var texto_fala = $Dialogo/TextoFala
+onready var texto_ajuda = $Dialogo/TextoAjuda
+onready var animacao = $Dialogo/AnimationPlayer
+
 func _ready():
-	$LabelAviso.visible = false
-	$Panel.visible = false
+	texto_ajuda.visible = false
+	texto_fala.visible = false
 
 func _process(_delta):
-	if jogador_perto and Input.is_action_just_pressed("interact") and not dialogo_ativo:
-		mostrar_fala()
-		dialogo_ativo = true
-		$LabelAviso.visible = false
-
-	elif dialogo_ativo and Input.is_action_just_pressed("next_dialogue"):
-		avancar_fala()
+	if jogador_perto:
+		if dialogo_ativo:
+			texto_ajuda.visible = false
+			return;
+		
+		texto_ajuda.visible = true
+		
+		if Input.is_action_just_pressed("interact"):
+			mostrar_fala()
+			dialogo_ativo = true
+		
+		return;
+	
+	texto_ajuda.visible = false
 
 func mostrar_fala():
-	$Panel.visible = true
-	$Panel/Label.text = falas[indice_fala]
+	texto_fala.text = falas[indice_fala]
+	animacao.play("dialogo")
 
 func avancar_fala():
 	indice_fala += 1
 	if indice_fala < falas.size():
 		mostrar_fala()
 	else:
-		$Panel.visible = false
+		texto_fala.visible = false
 		dialogo_ativo = false
 		indice_fala = 0
 
 func _on_Detector_body_entered(body):
-	if body.name == "Jogador":  
+	if body.is_in_group("player"):
 		jogador_perto = true
-		$LabelAviso.visible = true
 
 func _on_Detector_body_exited(body):
-	if body.name == "Jogador":
+	if body.is_in_group("player"):
 		jogador_perto = false
-		$LabelAviso.visible = false
-		$Panel.visible = false
-		dialogo_ativo = false
-		indice_fala = 0
