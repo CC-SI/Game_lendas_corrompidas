46aeb445dbc147210225bf134afd339752e6b65f
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-04-27 23:51:23 -0300

    update: adicionando um canvas layer para o dialogo da tayrin

--- tayrin.gd
+++ tayrin.gd
@@ -3,9 +3,9 @@ extends Node2D
 var dialogo_ativo = false
 
 func _ready():
-	$Panel.visible = false
-	$Panel/BotaoAjudaCombate.connect("pressed", self, "_on_BotaoAjudaCombate_pressed")
-	$Panel/BotaoAjudaMovimentacao.connect("pressed", self, "_on_BotaoAjudaMovimentacao_pressed")
+	$CanvasLayer/Panel.visible = false
+	$CanvasLayer/Panel/BotaoAjudaCombate.connect("pressed", self, "_on_BotaoAjudaCombate_pressed")
+	$CanvasLayer/Panel/BotaoAjudaMovimentacao.connect("pressed", self, "_on_BotaoAjudaMovimentacao_pressed")
 
 func _process(_delta):
 	if Input.is_action_just_pressed("falar_tayrin") and not dialogo_ativo:
@@ -13,26 +13,26 @@ func _process(_delta):
 
 func abrir_dialogo():
 	dialogo_ativo = true
-	$Panel.visible = true
-	$Panel/TextoDialogo.text = "Olá! Como posso te ajudar?"
-	$Panel/BotaoAjudaCombate.visible = true
-	$Panel/BotaoAjudaMovimentacao.visible = true
+	$CanvasLayer/Panel.visible = true
+	$CanvasLayer/Panel/TextoDialogo.text = "Olá! Como posso te ajudar?"
+	$CanvasLayer/Panel/BotaoAjudaCombate.visible = true
+	$CanvasLayer/Panel/BotaoAjudaMovimentacao.visible = true
 
 func _on_BotaoAjudaCombate_pressed():
-	$Panel/TextoDialogo.text = "Use ENTER para atacar e E para uivar!"
-	$Panel/BotaoAjudaCombate.visible = false
-	$Panel/BotaoAjudaMovimentacao.visible = false
+	$CanvasLayer/Panel/TextoDialogo.text = "Use ENTER para atacar e E para uivar!"
+	$CanvasLayer/Panel/BotaoAjudaCombate.visible = false
+	$CanvasLayer/Panel/BotaoAjudaMovimentacao.visible = false
 	yield(get_tree().create_timer(2.0), "timeout")
 	fechar_dialogo()
 
 func _on_BotaoAjudaMovimentacao_pressed():
-	$Panel/TextoDialogo.text = "Use WASD para se mover e SHIFT para correr!"
-	$Panel/BotaoAjudaCombate.visible = false
-	$Panel/BotaoAjudaMovimentacao.visible = false
+	$CanvasLayer/Panel/TextoDialogo.text = "Use WASD para se mover e SHIFT para correr!"
+	$CanvasLayer/Panel/BotaoAjudaCombate.visible = false
+	$CanvasLayer/Panel/BotaoAjudaMovimentacao.visible = false
 	yield(get_tree().create_timer(2.0), "timeout")
 	fechar_dialogo()
 
 func fechar_dialogo():
-	$Panel.visible = false
+	$CanvasLayer/Panel.visible = false
 	dialogo_ativo = false
 
