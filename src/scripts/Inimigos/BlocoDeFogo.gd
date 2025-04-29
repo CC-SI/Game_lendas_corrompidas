e81d4a31db127e18fd09b5e0de0bb5f63230f937
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-04-29 15:08:36 -0300

    fix: bloco de fogo tirando 1 de vida do player e com angulos ajustados

--- src/scripts/Inimigos/BlocoDeFogo.gd
+++ src/scripts/Inimigos/BlocoDeFogo.gd
@@ -4,31 +4,17 @@ var velocity = Vector2()
 var gravity = 600
 var jump_force = -400
 var has_landed = false
-var ground_y = 0
-
-onready var timer_destruir = Timer.new()
+var dano = 1
 
 func _ready():
 	randomize()
-
-	timer_destruir.wait_time = 5.0
-	timer_destruir.one_shot = true
-	timer_destruir.connect("timeout", self, "_destruir")
-	add_child(timer_destruir)
-	timer_destruir.start()
-
-	ground_y = position.y
-
-	if velocity == Vector2.ZERO:
-		var direction = 1
-		if randf() < 0.5:
-			direction = -1
-
-		var side_push = rand_range(40, 120) * direction
-		velocity = Vector2(side_push, jump_force)
+	# Define velocidade inicial como uma explosão para cima
+	var angle = rand_range(-PI / 3, -2 * PI / 3) # ângulo entre -60° e -120°
+	var speed = rand_range(100, 800)
+	velocity = Vector2(cos(angle), sin(angle)) * speed
 
 func set_direcao(nova_direcao):
-	velocity = nova_direcao.normalized() * 200
+	velocity = nova_direcao.normalized() * rand_range(400, 650)
 
 func _process(delta):
 	if has_landed:
@@ -39,17 +25,14 @@ func _process(delta):
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 
-	# Se tocou o chão
 	if not has_landed and is_on_floor():
 		has_landed = true
 
-func _destruir():
-	queue_free()
-
 
 func _on_Area2D_body_entered(body):
-	if (body.is_in_group("player")):
-		body.levar_dano(2)
-	
+	if body.is_in_group("player"):
+		DadosGlobais.vidas -= dano
+		print(DadosGlobais.vidas)
+
 	yield(get_tree().create_timer(1), "timeout")
 	queue_free()
