b0d390a51b1aa59db806fecbc1a61b0b57db0a6d
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-05-08 20:56:19 -0300

    refactor: usando o estado do morcego para setar as condições do animationtree

--- src/scripts/Inimigos/Morcego/animacao_morcego.gd
+++ src/scripts/Inimigos/Morcego/animacao_morcego.gd
@@ -3,6 +3,6 @@ extends Node2D
 onready var morcego = get_parent()
 
 func _process(delta):
-	$"../AnimationTree".set("parameters/conditions/esta_voando", !morcego.esta_atacando and !morcego.esta_morto)
-	$"../AnimationTree".set("parameters/conditions/esta_atacando", morcego.esta_atacando and !morcego.esta_morto)
+	$"../AnimationTree".set("parameters/conditions/esta_voando", morcego.estado != "perseguindo" and !morcego.esta_morto)
+	$"../AnimationTree".set("parameters/conditions/esta_atacando", morcego.estado == "perseguindo" and !morcego.esta_morto)
 	$"../AnimationTree".set("parameters/conditions/esta_morto", morcego.esta_morto)
