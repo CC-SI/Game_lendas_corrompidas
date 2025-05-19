d48ca5995defdd1a8cfe1f9a3f59c5b6cc5707c1
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-05-19 04:46:10 -0300

    raw: criando animação de teletransporte e escudo ativando

---
+++ src/scripts/Inimigos/Curupira/animacao_curupira.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+onready var curupira = get_parent()
+
+func _process(delta):
+	$"../AnimationTree".set("parameters/assobio/conditions/esta_parado", curupira.estado == "parado")
+	$"../AnimationTree".set("parameters/bola_fogo/conditions/esta_parado", curupira.estado == "parado")
+	$"../AnimationTree".set("parameters/cansado/conditions/esta_parado", curupira.estado == "parado")
+	$"../AnimationTree".set("parameters/conditions/assobio", curupira.estado == "assobiando")
+	$"../AnimationTree".set("parameters/conditions/bola_fogo", curupira.estado == "bola_fogo")
+	$"../AnimationTree".set("parameters/conditions/esta_cansado", curupira.estado == "descansando")
+	$"../AnimationTree".set("parameters/conditions/soco", curupira.estado == "soco")
+	$"../AnimationTree".set("parameters/conditions/esta_teletraportando", curupira.estado == "teletransportando")
+	$"../AnimationTree".set("parameters/conditions/esta_morto", curupira.esta_morto)
