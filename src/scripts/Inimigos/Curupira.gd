c5499423a28cdc88e9db342564fa53df08a4abb4
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-05-20 14:02:39 -0300

    update: curupira com comportamentos refeitos

--- src/scripts/Inimigos/Curupira.gd
+++ src/scripts/Inimigos/Curupira.gd
@@ -1,158 +1,166 @@
-extends InimigoBase
+extends KinematicBody2D
 
 onready var sprite = $Sprite
-onready var kickArea2D = $Chute
+onready var area_soco = $Soco
+onready var area_decisao = $ZonaDecisao
 
-onready var posTop = $PositionTop
-onready var posEsq = $PositionEsq
-onready var posDir = $PositionDir
+onready var posicoes_bola_fogo = [
+	$PositionTop,
+	$PositionEsq,
+	$PositionDir
+]
 
-onready var timer = $Timer
-
-onready var lifeBar = $ProgressBar
-
-onready var zona1 = $"Zona 1"
-onready var zona2 = $"Zona 2"
-onready var zona3 = $"Zona 3"
+var bolas_fogo = []
+var bolas_fogo_restantes = []
 
 onready var bgm = $BGM
+
+var gravidade = 800
 
 var playerList = []
 
 var lado
-var current_attacks: int
-var isAssobioOpen: bool = false
+
+#estados
+var estado = "parado"
+var pode_atacar = false
+var escudo_ativo = false
+var bravo = false
+var ataques_realizados: int
 var isOutZone: bool = true
-var isAttacking = false
-var last_attack = ""
-var same_attack_count = 0
-var can_attack = false
-var isAssobio = false
-var isPostAssobio = false
-var is_parado: bool = false 
+var attack_choice = ""
+
+#intervalos de tempo
+var intervalo_ataque
+var intervalo_bola_fogo
+var intervalo_assobio
+
+export var vidas = 20
+
+var contagem_ondas_sonoras = 0
 
 func _ready():
-	vidas = 10
-	barra_de_vida.value = vidas
-	timer.start(2)
-	disable_area2D()
-	velocidade = 300
+	iniciar_timer_ataque()
 	bgm.play(0);
 
-func _process(delta):
-	print(vidas)
+func _physics_process(delta):
+	var direcao = Vector2.ZERO
+	direcao.y += gravidade
 	
-	follow_player()
+	direcao = move_and_slide(direcao, Vector2.UP)
+	
+	if estado == "morto":
+		return
+	
+	var position_target = DadosGlobais.player.global_position
+	lado = 1 if global_position.x < position_target.x else -1
 	mudarLadoSprite()
-
-	if not isAttacking and isOutZone and can_attack and not is_parado:
-		decides_to_attack()
 	
-	if isAssobio:
-		assobio()
-		
-	if not is_parado:
-		ativar_defesa(true)
-	else:
+	if estado == "descansando":
 		ativar_defesa(false)
-
-	if current_attacks >= 5:
-		isAssobio = true
-		print("Assobio on")
-		yield(get_tree().create_timer(3), "timeout")
-		is_parado = true
-		velocidade = 0
-		current_attacks = 0
 	else:
-		isAssobio = false
+		ativar_defesa(true)
+	
+	if estado == "parado" and pode_atacar:
+		decides_to_attack()
 
 func decides_to_attack():
-	var attack_choice = ""
-	isAssobio = false
+	if ataques_realizados >= 3:
+		estado = "assobiando"
+		ataques_realizados = 0
+		return
 	
-	if last_attack == "tp":
-		attack_choice = "bola_de_fogo"
-	elif last_attack == "bola_de_fogo":
-		attack_choice = "tp"
-	else:
-		attack_choice = "tp" if rand_range(0, 1) > 0.5 else "bola_de_fogo"
-		
-	if same_attack_count >= 2:
-		attack_choice = "tp" if last_attack != "tp" else "bola_de_fogo"
-		same_attack_count = 0	
+	pode_atacar = false
+	ataques_realizados += 1
 	
-	if attack_choice == "tp":
-		tp()
-		current_attacks += 1
-	elif attack_choice == "bola_de_fogo":
-		_bola_de_fogo()
-		current_attacks += 1
-	else:
-		same_attack_count = 1
+	attack_choice = "soco" if rand_range(0, 1) > 0.5 else "bola_fogo"
 	
-	last_attack = attack_choice
+	if !isOutZone:
+		atacar()
+		return
 	
-	print(current_attacks)
+	tp()
 
-	
+func iniciar_timer_ataque():
+	$AtaqueTimer.start(intervalo_ataque)
 
-func assobio():
-	if isAssobio and not is_parado:
-		
-		# Ativa as zonas de ataque
-		yield(get_tree().create_timer(5), "timeout")
-		enable_area2D()
-		
-		# Desativa as zonas de ataque 
-		yield(get_tree().create_timer(5), "timeout")
-		disable_area2D()
-		
-		# Descansando
-		yield(get_tree().create_timer(5), "timeout")
-		current_attacks = 0
-		isAssobio = false
-		is_parado = false
-		velocidade = 300
-		
+func atacar():
+	estado = attack_choice
+
+func ativar_defesa(isActive):
+	escudo_ativo = isActive
+
+func levar_dano(valor):
+	if escudo_ativo:
+		if $Escudo/AnimationPlayer.is_playing():
+			$Escudo/AnimationPlayer.stop()
+		$Escudo/AnimationPlayer.play("escudo_ativando")
+		return
+	
+	var material = $Sprite.material
+	material.set_shader_param("flash", true)
+	yield(get_tree().create_timer(0.25), "timeout")
+	material.set_shader_param("flash", false)
+	
+	vidas -= valor
+	$BarraVidaCurupira.diminuir_vida(valor)
+	
+	if vidas <= 10:
+		bravo = true
+	
+	if vidas <= 0:
+		morrer()
+
+func tp_player():
+	var position_target = DadosGlobais.player.global_position
+	var direction = 1 if global_position.x < position_target.x else -1
+	var teleport_position = position_target + Vector2(60 * direction, 0)
+	global_position = teleport_position
+
+func aplicar_dano(valor):
+	var player = get_tree().get_nodes_in_group("player")
+	if (player.size() > 0):
+		player[0].levar_dano(valor)
+
+func resetar_estado():
+	estado = "parado"
+	intervalo_ataque = 1 if bravo else 2
+	intervalo_assobio = 1.5 if bravo else 2.5
+	intervalo_bola_fogo = 0.5 if bravo else 1.5
+
+func descansar():
+	$DescansoTimer.start(6)
+
+func assobiar():
+	criar_onda_sonora()
+	$AssobioTimer.start(intervalo_assobio)
+
+func criar_onda_sonora():
+	contagem_ondas_sonoras += 1
+	var onda_sonora = preload("res://src/Cenas/Inimigos/Curupira/OndaSonora.tscn").instance()
+	get_parent().add_child(onda_sonora)
+	onda_sonora.global_position = $PosicaoAssobio.global_position
+
 func tp():
-	isAttacking = true
-	tp_player()
-	if not is_parado:
-		velocidade = 1
-	yield(get_tree().create_timer(1), "timeout")
-	if not is_parado:
-		velocidade = 300
-	
-	isAttacking = false
-	can_attack = false
-	timer.start(2)
+	estado = "teletransportando"
 
-func _bola_de_fogo():
-	var intervalBetweenBolls = rand_range(0.5, 1.2)
-	var bolaDeFogoTop = preload("res://src/Cenas/Inimigos/Bolas de fogo/BolaDeFogoCurupira.tscn")
-	var bolaDeFogoEsq = preload("res://src/Cenas/Inimigos/Bolas de fogo/BolaDeFogoEsq.tscn")
-	var bolaDeFogoDir = preload("res://src/Cenas/Inimigos/Bolas de fogo/BolaDeFogoDir.tscn")
-	
-	isAttacking = true
-	
-	var bolaTop = bolaDeFogoTop.instance()
-	get_parent().add_child(bolaTop)
-	bolaTop.global_position = posTop.global_position
-	yield(get_tree().create_timer(intervalBetweenBolls), "timeout")
-	
-	var bolaEsq = bolaDeFogoEsq.instance()
-	get_parent().add_child(bolaEsq)
-	bolaEsq.global_position = posEsq.global_position
-	yield(get_tree().create_timer(intervalBetweenBolls), "timeout")
-	
-	var bolaDir = bolaDeFogoDir.instance()
-	get_parent().add_child(bolaDir)
-	bolaDir.global_position = posDir.global_position
-	
-	yield(get_tree().create_timer(1.0), "timeout")
-	isAttacking = false
-	can_attack = false
-	timer.start(2)
+func criar_bolas_fogo():
+	bolas_fogo.clear()
+	for posicao in posicoes_bola_fogo:
+		var bola_fogo = preload("res://src/Cenas/Inimigos/Bolas de fogo/BolaDeFogoCurupira.tscn").instance()
+		get_parent().add_child(bola_fogo)
+		bola_fogo.global_position = posicao.global_position
+		bolas_fogo.append(bola_fogo)
+
+func iniciar_lancamento_bola_fogo():
+	bolas_fogo_restantes.clear()
+	bolas_fogo_restantes = bolas_fogo.duplicate()
+	lancar_bola_fogo()
+	$BolaFogoTimer.start(intervalo_bola_fogo)
+
+func lancar_bola_fogo():
+	var bola_fogo = bolas_fogo_restantes.pop_front()
+	bola_fogo.lancar()
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
@@ -166,55 +174,37 @@ func _on_Area2D_body_exited(body):
 
 func _on_Chute_body_entered(body):
 	if body.is_in_group("player"):
-		current_attacks += 1
-		aplicar_dano(1)
-		print("Ataques: ", current_attacks) 
+		ataques_realizados += 1
+		aplicar_dano(2)
 
 func mudarLadoSprite():
 	sprite.flip_h = lado != 1
-	kickArea2D.scale.x = lado
+	area_soco.scale.x = lado
+	$PosicaoAssobio.position.x = abs($PosicaoAssobio.position.x) * lado
 
 func _on_Timer_timeout():
-	can_attack = true
-
-func _on_Zona_1_body_entered(body):
-	if body.is_in_group("player"):
-		aplicar_dano(1)
-
-func _on_Zona_2_body_entered(body):
-	if body.is_in_group("player"):
-		aplicar_dano(1)
-
-func _on_Zona_3_body_entered(body):
-	if body.is_in_group("player"):
-		aplicar_dano(1)
-
-func disable_area2D():
-	zona1.visible = false
-	zona1.monitoring = false
-	zona1.set_process(false)
-
-	zona2.visible = false
-	zona2.monitoring = false
-	zona2.set_process(false)
-
-	zona3.visible = false
-	zona3.monitoring = false
-	zona3.set_process(false)
-	
-func enable_area2D():
-	zona1.visible = true
-	zona1.monitoring = true
-	zona1.set_process(true)
-
-	zona2.visible = true
-	zona2.monitoring = true
-	zona2.set_process(true)
-
-	zona3.visible = true
-	zona3.monitoring = true
-	zona3.set_process(true)
+	pode_atacar = true
 
 func morrer():
-	.morrer();
+	estado = "morto"
 	bgm.stop();
+
+func _on_AssobioTimer_timeout():
+	if contagem_ondas_sonoras >= 3:
+		contagem_ondas_sonoras = 0
+		$AssobioTimer.stop()
+		estado = "descansando"
+		return
+	
+	criar_onda_sonora()
+
+func _on_DescansoTimer_timeout():
+	estado = "parado"
+
+func _on_BolaFogoTimer_timeout():
+	if bolas_fogo_restantes.empty():
+		$BolaFogoTimer.stop()
+		estado = "parado"
+		return
+	
+	lancar_bola_fogo()
