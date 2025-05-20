89703a17d867b106e3ecb6af0a250215d1abd459
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-05-20 14:00:38 -0300

    update: controlando a condição dos estados de animação

--- src/scripts/Inimigos/Curupira/animacao_curupira.gd
+++ src/scripts/Inimigos/Curupira/animacao_curupira.gd
@@ -3,12 +3,12 @@ extends Node2D
 onready var curupira = get_parent()
 
 func _process(delta):
-	$"../AnimationTree".set("parameters/assobio/conditions/esta_parado", curupira.estado == "parado")
 	$"../AnimationTree".set("parameters/bola_fogo/conditions/esta_parado", curupira.estado == "parado")
 	$"../AnimationTree".set("parameters/cansado/conditions/esta_parado", curupira.estado == "parado")
+	$"../AnimationTree".set("parameters/assobio/conditions/esta_cansado", curupira.estado == "descansando")
 	$"../AnimationTree".set("parameters/conditions/assobio", curupira.estado == "assobiando")
 	$"../AnimationTree".set("parameters/conditions/bola_fogo", curupira.estado == "bola_fogo")
 	$"../AnimationTree".set("parameters/conditions/esta_cansado", curupira.estado == "descansando")
 	$"../AnimationTree".set("parameters/conditions/soco", curupira.estado == "soco")
-	$"../AnimationTree".set("parameters/conditions/esta_teletraportando", curupira.estado == "teletransportando")
-	$"../AnimationTree".set("parameters/conditions/esta_morto", curupira.esta_morto)
+	$"../AnimationTree".set("parameters/conditions/esta_teletransportando", curupira.estado == "teletransportando")
+	$"../AnimationTree".set("parameters/conditions/esta_morto", curupira.estado == "morto")
