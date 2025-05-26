8beefad104eec91a57e052b9f3596feb1fa8ac85
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-05-26 17:34:18 -0300

    feat: criando a cena do menu principal

---
+++ src/scripts/Menus/menu_principal.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+onready var camera = $Camera2D
+onready var transicao = $Animacao/AnimationPlayer
+
+export var velocidade_camera = 100
+
+func _ready():
+	transicao.play_backwards("transicao_preta")
+
+func _process(delta):
+	camera.position.x += velocidade_camera * delta
+
+func transicao_para_cutscene_introducao():
+	transicao.play("transicao_preta")
+	yield(transicao, "animation_finished")
+	get_tree().change_scene("res://src/Cenas/Cutscene.tscn")
+
+func sair_jogo():
+	transicao.play("transicao_preta")
+	yield(transicao, "animation_finished")
+	get_tree().quit()
+
+func _on_Botao_Jogar_pressed():
+	if transicao.is_playing():
+		return
+	transicao_para_cutscene_introducao()
+
+func _on_Botao_Sair_pressed():
+	if transicao.is_playing():
+		return
+	sair_jogo()
