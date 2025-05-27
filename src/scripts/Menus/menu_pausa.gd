6eefd9e5fd385b708c5e7871e6fba89b2b7db942
Ryushuke <ryushuke@hotmail.com>
2025-05-27 11:58:19 -0300

    update: Adicionado som aos botões do menu de pause.
    - Adicionado som de hover em todos os botões.

--- src/scripts/Menus/menu_pausa.gd
+++ src/scripts/Menus/menu_pausa.gd
@@ -1,10 +1,14 @@
 extends Control
 
+onready var cliqueASP = $Som/Clicar
+
 signal continuar_pressed
 signal menu_pressed
 
 func _on_Botao_Continuar_pressed():
+	cliqueASP.play(0)
 	emit_signal("continuar_pressed")
 
 func _on_Botao_Menu_pressed():
+	cliqueASP.play(0)
 	emit_signal("menu_pressed")
