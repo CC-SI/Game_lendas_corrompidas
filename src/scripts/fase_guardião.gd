485e5ca55808512e621c8d9428d1f45c0f6d7b6f
Gabriel Matos Alencar <gabrielalencar.gma@gmail.com>
2025-05-27 17:44:40 -0300

    feat: adicionando uma cutscene para o inicio da luta entre curupira e jogador

--- src/scripts/fase_guardião.gd
+++ src/scripts/fase_guardião.gd
@@ -1,6 +1,22 @@
 extends Node2D
 
-var estado = "gameplay"
+onready var animacao = $Animacao/AnimationPlayer
+onready var player = $Player/Player
+onready var curupira = $Curupira/Curupira
+
+var estado = "cutscene"
 
 func _ready():
 	$CanvasLayer.associar_fase(self)
+	iniciar_cutscene("comeco_luta")
+
+func iniciar_cutscene(cutscene):
+	estado = "cutscene"
+	player.cutscene = true
+	curupira.cutscene = true
+	animacao.play(cutscene)
+
+func terminar_cutscene():
+	estado = "gameplay"
+	player.cutscene = false
+	curupira.cutscene = false
